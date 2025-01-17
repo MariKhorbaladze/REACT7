6fdec69c4dca4a6bcee7be4559701836c52a82ff
MariKhorbaladze <129887035+MariKhorbaladze@users.noreply.github.com>
2025-01-17 23:41:32 +0400

    latest version

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
-  base: '/REACT7/', 
-  plugins: [react()]
+  plugins: [react()],
 })
+
+
